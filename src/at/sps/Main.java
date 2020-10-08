b62cfa5fe2071cea9c2d1389c83e124ffa2cb19e
Simon Sperr <simon.sperr@htl-wels.at>
2020-10-08 10:30:21 +0200

    Simple Addition

--- src/at/sps/Main.java
+++ src/at/sps/Main.java
@@ -1,4 +1,32 @@
 package at.sps;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double sum1 = 0;
+        double sum2 = 0;
+        double sum;
+
+        System.out.println("Geben Sie zwei Zahlen ein um diese zu addieren!");
+
+        do
+        {
+
+            try
+            {
+                System.out.printf("Erste Zahl:");
+                sum1 = sc.nextDouble();
+                System.out.printf("Zweite Zahl:");
+                sum2 = sc.nextDouble();
+            } catch (Exception e) {
+                System.out.println("Bei der Eingabe ist ein Fehler aufgetreten, versuchen Sie es Erneut!");
+            }
+        } while(sum1 == 0 && sum2 == 0);
+
+        sum = sum1 + sum2;
+        System.out.println("Ergebniss: " + sum);
+
+    }
 }
