ed7891822c3e85ad7c1fd85a306e665daa8deeb9
Simon Sperr <simon.sperr@htl-wels.at>
2020-10-08 11:13:13 +0200

    Multiple Numbers

--- src/at/sps/Main.java
+++ src/at/sps/Main.java
@@ -4,29 +4,43 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
         double sum1 = 0;
-        double sum2 = 0;
-        double sum;
+        String ans = "";
+        boolean adding = true;
         boolean run = true;
 
         while (run)
         {
-            System.out.println("Geben Sie zwei Zahlen ein um diese zu addieren!");
+            System.out.println("Geben Sie Zahlen ein um diese mit = zu addieren!");
             try {
-                System.out.printf("Erste Zahl: ");
-                sum1 = sc.nextDouble();
-                System.out.printf("Zweite Zahl: ");
-                sum2 = sc.nextDouble();
-                sum = sum1 + sum2;
-                System.out.println("Ergebniss: " + sum);
+                int c = 1;
+                while (adding)
+                {
+                    Scanner sc = new Scanner(System.in);
+                    System.out.printf("%d. Zahl: ", c);
+                    ans = sc.next();
+                    try
+                    {
+                        sum1 += Double.parseDouble(ans);
+                    }
+                    catch(Exception e)
+                    {
+                        if(ans.equals("="))
+                            adding = false;
+                        else
+                            System.out.println("Keine Zahl! Eingabe wird nicht gewertet.");
+                    }
+                    c++;
+                }
+
             } catch (Exception e) {
                 System.out.println("Bei der Eingabe ist ein Fehler aufgetreten, versuchen Sie es Erneut!");
-                sc.reset();
             }
 
+            System.out.println("Ergebniss: " + sum1);
+            Scanner sc = new Scanner(System.in);
             System.out.println("Möchten Sie das Programm beenden? (y/n)");
-            String ans = sc.next();
+            ans = sc.next();
             if (ans.equals("y"))
             {
                 run = false;
